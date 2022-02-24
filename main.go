b012c8bb92ea0ab88e57caedaf4e2508d2abde9d
Jean Catanho <jean.catanho@g.globo>
2022-02-24 00:20:04 -0300

    Add json lyrics and printing randomly

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	rand.Seed(time.Now().UnixNano())
+
+	songJson, err := os.Open("albums/the_wicked_symphony/1_the_wicked_symphony.json")
+	if err != nil {
+		fmt.Printf("Error: %+v", err)
+	}
+
+	decoder := json.NewDecoder(songJson)
+
+	var song Song
+	err = decoder.Decode(&song)
+	if err != nil {
+		fmt.Printf("Error: %+v", err)
+	}
+
+	fmt.Printf("%s\n", song.getLyric())
+}
+
+type Song struct {
+	Name   string   `json:"name"`
+	Album  string   `json:"album"`
+	Lyrics []string `json:"lyrics"`
+}
+
+func (s *Song) getLyric() string {
+	return s.Lyrics[rand.Intn(len(s.Lyrics))]
 }
