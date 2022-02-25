6ed8720a254badef81bbb72864e81b03b076fa01
Jean Catanho <jean.catanho@g.globo>
2022-02-24 23:48:17 -0300

    First version

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/jensmcatanho/avantasia-txt
 
 go 1.17
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20211115160449-93a8679adecb
+	github.com/dghubble/oauth1 v0.7.1
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"time"
 
+	"github.com/jensmcatanho/avantasia-txt/client"
 	"github.com/jensmcatanho/avantasia-txt/models"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	twitterClient := client.NewTwitterClient(client.Credentials{
+		ConsumerKey:       os.Getenv("CONSUMER_KEY"),
+		ConsumerSecret:    os.Getenv("CONSUMER_SECRET"),
+		AccessToken:       os.Getenv("ACCESS_TOKEN"),
+		AccessTokenSecret: os.Getenv("ACCESS_TOKEN_SECRET"),
+	})
 
-	songJson, err := os.Open("albums/the_wicked_symphony/1_the_wicked_symphony.json")
+	song, err := getRandomSong()
 	if err != nil {
-		fmt.Printf("Error: %+v", err)
+		log.Printf("Error: %+v", err)
+	}
+
+	log.Printf("Song: %s\n", song.Name)
+	err = twitterClient.Tweet(song.GetLyric())
+	if err != nil {
+		log.Printf("Error: %+v", err)
+
+	}
+}
+
+func getRandomSong() (*models.Song, error) {
+	album := "the_wicked_symphony"
+
+	songFiles, err := ioutil.ReadDir(fmt.Sprintf("albums/%s", album))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	songFile := songFiles[rand.Intn(len(songFiles))]
+	songJson, err := os.Open(fmt.Sprintf("albums/%s/%s", album, songFile.Name()))
+	if err != nil {
+		return nil, err
 	}
 
 	decoder := json.NewDecoder(songJson)
@@ -23,8 +54,8 @@ func main() {
 	var song models.Song
 	err = decoder.Decode(&song)
 	if err != nil {
-		fmt.Printf("Error: %+v", err)
+		return nil, err
 	}
 
-	fmt.Printf("%s\n", song.GetLyric())
+	return &song, nil
 }
